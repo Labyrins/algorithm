614be5c9135e89825cdd04275c6f91656b4cc40d
HongsikLee <labyrins@gmail.com>
2015-04-15 01:38:35 +0900

    modify selectsort.java

--- src/algorithm/SelectSort.java
+++ src/algorithm/SelectSort.java
@@ -13,18 +13,26 @@ public class SelectSort {
         for(int i=0; i<input; i++){
             array[i] = sc.nextInt();
         }
-        int min;
-        for(int i=0; i<input; i++){
-            
-//            if(misn<=array[i])
-            
-            
-            
+        int tempA, tempB;
+        for(int y=input; y>0; y--){
+        	int min = array[input-y];
+	        for(int i=input-y+1; i<input; i++){
+	            if(min>=array[i]){
+	            	tempA = array[input-y];
+	            	tempB = array[i];
+	            	array[input-y] = tempB;
+	            	array[i] = tempA;
+	            }
+	            
+	        }
+	        for(int x=0; x<input; x++){
+	        	System.out.print(array[x]+ " ");
+	        }
+	        System.out.println();
+	        
         }
-//        for(int x=0; x<input; x++){
-//            System.out.print(array[x]+ " ");
-//        }
-//        System.out.println();
+
+        System.out.println();
 
     }
 
