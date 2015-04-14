80fd38a114a243abebe746f5dd561fe39f5c126c
Hongsik Alex Lee <wow.lee@samsung.com>
2015-04-14 18:54:08 +0900

    3rd commit

---
+++ src/algorithm/SelectSort.java
@@ -0,0 +1,31 @@
+package algorithm;
+
+import java.util.Scanner;
+
+
+public class SelectSort {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int array[] = new int[input];
+        
+        for(int i=0; i<input; i++){
+            array[i] = sc.nextInt();
+        }
+        int min;
+        for(int i=0; i<input; i++){
+            
+//            if(misn<=array[i])
+            
+            
+            
+        }
+//        for(int x=0; x<input; x++){
+//            System.out.print(array[x]+ " ");
+//        }
+//        System.out.println();
+
+    }
+
+}
